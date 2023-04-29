9967926740bbdbf08bbcdb1d7980839036013a41
mayu_kishimoto <nhpw.29@gmail.com>
2023-04-29 18:12:44 +0900

    add: System Specの作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,7 +13,7 @@ class TasksController < ApplicationController
       render :new
     else
       if @task.save
-        redirect_to tasks_path, notice: "タスクを作成しました！"
+        redirect_to task_path(@task.id), notice: "タスクを作成しました！"
       else
         render :new
       end
