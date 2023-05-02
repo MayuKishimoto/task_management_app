45a729361c17c50ada3523dfde85f010fbff561a
mayu_kishimoto <nhpw.29@gmail.com>
2023-05-02 09:57:18 +0900

    add: System Specテストの追加

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,10 +1,10 @@
 require "rails_helper"
 RSpec.describe "タスク管理機能", type: :system do
-  before do
-    # あらかじめタスク一覧のテストで使用するためのタスクを二つ作成する
-    FactoryBot.create(:task)
-    FactoryBot.create(:second_task)
-  end
+  # before do
+  #   # あらかじめタスク一覧のテストで使用するためのタスクを二つ作成する
+  #   FactoryBot.create(:task)
+  #   FactoryBot.create(:second_task)
+  # end
   describe "新規作成機能" do
     context "タスクを新規作成した場合" do
       it "作成したタスクが表示される" do
@@ -14,16 +14,17 @@ RSpec.describe "タスク管理機能", type: :system do
         # 2. 新規登録内容を入力する
         #「タスク名」というラベル名の入力欄と、「タスク詳細」というラベル名の入力欄にタスクのタイトルと内容をそれぞれ入力する
         # ここに「タスク名」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
-
+        fill_in 'Title', with: 'タイトル'
         # ここに「タスク詳細」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
-
+        fill_in 'Content', with: 'コンテント'
         # 3. 「登録する」というvalue（表記文字）のあるボタンをクリックする
         # ここに「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）する処理を書く
-        
+        click_on '登録'
         # 4. clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
         # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
         # ここにタスク詳細ページに、テストコードで作成したデータがタスク詳細画面にhave_contentされているか（含まれているか）を確認（期待）するコードを書く
-        
+        expect(page).to have_content 'タイトル'
+        expect(page).to have_content 'コンテント'
       end
     end
   end
@@ -34,8 +35,6 @@ RSpec.describe "タスク管理機能", type: :system do
         task = FactoryBot.create(:task, title: 'task')
         # タスク一覧ページに遷移
         visit tasks_path
-        # ページ遷移直後でデバッグ
-        # binding.irb
         # タスク一覧ページに遷移できているかを確認
         current_path
         # タスクがデータベースに作成されているかを確認
@@ -49,6 +48,15 @@ RSpec.describe "タスク管理機能", type: :system do
   describe "詳細表示機能" do
     context "任意のタスク詳細画面に遷移した場合" do
       it "該当タスクの内容が表示される" do
+        # テストで使用するためのタスクを作成
+        task = FactoryBot.create(:task, title: 'task')
+        # タスク一覧ページに遷移
+        visit tasks_path
+        # 「詳細」というvalue（表記文字）のあるボタンをクリックする
+        click_on '詳細'
+        # 表示するページにタスク情報が入っているかを確認
+        page.html
+        expect(page).to have_content 'task'
       end
     end
   end
