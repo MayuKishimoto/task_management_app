549c01751bcf15fc3f638d7a8fc3cc8dfd602301
mayu_kishimoto <nhpw.29@gmail.com>
2023-05-03 16:44:42 +0900

    update: tasks/indexの設定変更、pgのバージョンアップ

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,10 +1,6 @@
 require "rails_helper"
 RSpec.describe "タスク管理機能", type: :system do
-  # before do
-  #   # あらかじめタスク一覧のテストで使用するためのタスクを二つ作成する
-  #   FactoryBot.create(:task)
-  #   FactoryBot.create(:second_task)
-  # end
+  let!(:task) { FactoryBot.create(:task, title: 'task') }
   describe "新規作成機能" do
     context "タスクを新規作成した場合" do
       it "作成したタスクが表示される" do
@@ -29,12 +25,16 @@ RSpec.describe "タスク管理機能", type: :system do
     end
   end
   describe "一覧表示機能" do
+    before do
+      # 「一覧画面に遷移した場合」や「タスクが作成日時の降順に並んでいる場合」など、contextが実行されるタイミングで、before内のコードが実行される
+      visit tasks_path
+    end
     context "一覧画面に遷移した場合" do
       it "作成済みのタスク一覧が表示される" do
-        # テストで使用するためのタスクを作成
-        task = FactoryBot.create(:task, title: 'task')
-        # タスク一覧ページに遷移
-        visit tasks_path
+        # # テストで使用するためのタスクを作成
+        # task = FactoryBot.create(:task, title: 'task')
+        # # タスク一覧ページに遷移
+        # visit tasks_path
         # タスク一覧ページに遷移できているかを確認
         current_path
         # タスクがデータベースに作成されているかを確認
@@ -44,15 +44,21 @@ RSpec.describe "タスク管理機能", type: :system do
         expect(page).to have_content 'task'
       end
     end
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        # ここに実装する
+      end
+    end
   end
   describe "詳細表示機能" do
     context "任意のタスク詳細画面に遷移した場合" do
       it "該当タスクの内容が表示される" do
-        # テストで使用するためのタスクを作成
-        task = FactoryBot.create(:task, title: 'task')
+        # # テストで使用するためのタスクを作成
+        # task = FactoryBot.create(:task, title: 'task')
         # タスク一覧ページに遷移
         visit tasks_path
         # 「詳細」というvalue（表記文字）のあるボタンをクリックする
+        # binding.irb
         click_on '詳細'
         # 表示するページにタスク情報が入っているかを確認
         page.html
