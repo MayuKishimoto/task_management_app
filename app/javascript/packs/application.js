0d53ee9d16800e65de64e7d7ee9f3fd0bcafa97c
mayu_kishimoto <nhpw.29@gmail.com>
2023-05-07 15:33:29 +0900

    add :ボタンデザインの調整

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'bootstrap';
 import '../stylesheets/application';
+import '../stylesheets/task';
 
 Rails.start()
 Turbolinks.start()
