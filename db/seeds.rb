cf6d86f49552d308bb1126eb13c4371e12f1d9f6
mayu_kishimoto <nhpw.29@gmail.com>
2023-05-12 22:35:05 +0900

    add: seedデータの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-100.times do |n|
-  name = Faker::Games::Pokemon.name
-  email = Faker::Internet.email
-  password = "password"
-  User.create!(name: name,
-               email: email,
-               password: password,
+10.times do |n|
+  title = Faker::Games::Pokemon.name
+  content = "content"
+  expired_at = '2024-01-01 00:00:00'
+  status = '未着手'
+  priority = 1
+  user_id = 102
+  Task.create!(title: title,
+               content: content,
+               expired_at: expired_at,
+               status: status,
+               priority: priority,
+               user_id: user_id
                )
 end
