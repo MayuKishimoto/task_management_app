6972d2da43716dd193f3158cba151df6226ecf91
mayu_kishimoto <nhpw.29@gmail.com>
2023-05-13 10:10:55 +0900

    fix: マイグレーションファイルの修正

--- db/migrate/20230511021205_create_labellings.rb
+++ db/migrate/20230511021205_create_labellings.rb
@@ -1,7 +1,7 @@
 class CreateLabellings < ActiveRecord::Migration[6.1]
   def change
     create_table :labellings do |t|
-      t.references :label, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
       t.references :task, null: false, foreign_key: true
 
       t.timestamps
